7a573342f930f2bd6f0c4da2e4d4d163132fd408
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-10-28 20:17:58 -0600

    base project

---
+++ src/app/game/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Game = () => {
+    return (
+        <div>
+            <h2>Bienvenido a la página de inicio</h2>
+        </div>
+    );
+};
+
+export default Game;
